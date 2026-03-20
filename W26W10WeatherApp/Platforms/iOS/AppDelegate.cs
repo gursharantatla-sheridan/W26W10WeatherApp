3580bda0715b3130183edffecb14d62a4acefc14
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-20 09:38:57 -0400

    Add project files.

---
+++ W26W10WeatherApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace W26W10WeatherApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
